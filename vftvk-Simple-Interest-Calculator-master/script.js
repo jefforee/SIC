cb0342dfbbfd3b60255101165ac0af210426c012
Jeffrey Huang <jeffreyhuang@jeffreys-mbp.usca.ibm.com>
2022-07-05 12:45:48 -0700

    done

---
+++ vftvk-Simple-Interest-Calculator-master/script.js
@@ -0,0 +1,23 @@
+
+
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        alert("Enter a positive number.");
+        document.getElementById("principal").focus();
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    p = document.getElementById("principal").value;
+    document.getElementById("result").innerHTML="<br>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"</mark>%\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText= rateval + '%';
+}
